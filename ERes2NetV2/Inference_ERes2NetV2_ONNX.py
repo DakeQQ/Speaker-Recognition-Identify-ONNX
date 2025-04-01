11e20e5e5f492f92873244972584d9381c82baac
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-01 10:59:50 +0800

    Update Inference_ERes2NetV2_ONNX.py

--- ERes2NetV2/Inference_ERes2NetV2_ONNX.py
+++ ERes2NetV2/Inference_ERes2NetV2_ONNX.py
@@ -17,7 +17,7 @@ SIMILARITY_THRESHOLD = 0.5              # Threshold to determine the speaker's i
 
 
 def normalize_to_int16(audio):
-    max_val = np.max(np.abs(audio.astype(np.float32)))
+    max_val = np.max(np.abs(audio))
     scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
     return (audio * float(scaling_factor)).astype(np.int16)
 
@@ -71,7 +71,7 @@ if "float16" in model_type:
 for test in test_audio:
     print("----------------------------------------------------------------------------------------------------------")
     print(f"\nTest Input Audio: {test}")
-    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int32)
+    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
     audio = normalize_to_int16(audio)
     audio_len = len(audio)
     audio = audio.reshape(1, 1, -1)
