0636b478207ca10a38c4a74d5a5db24a022d3ca2
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-15 10:09:05 +0800

    Update Export_CAM++_ONNX.py

--- CAM++_Transformer/Export_CAM++_ONNX.py
+++ CAM++_Transformer/Export_CAM++_ONNX.py
@@ -18,15 +18,26 @@ test_audio = model_path + "/examples/scl_example2.wav"                          
 ORT_Accelerate_Providers = []                               # If you have accelerate devices for : ['CUDAExecutionProvider', 'TensorrtExecutionProvider', 'CoreMLExecutionProvider', 'DmlExecutionProvider', 'OpenVINOExecutionProvider', 'ROCMExecutionProvider', 'MIGraphXExecutionProvider', 'AzureExecutionProvider']
                                                             # else keep empty.
 DYNAMIC_AXES = True                                         # The default dynamic_axes is the input audio length. Note that some providers only support static axes.
-INPUT_AUDIO_LENGTH = 160000 if not DYNAMIC_AXES else 16000  # Set for static axis export: the length of the audio input signal (in samples). Iy use DYNAMIC_AXES, Default to 320000, you can adjust it.
+INPUT_AUDIO_LENGTH = 160000                                 # The maximum input audio length.
 WINDOW_TYPE = 'kaiser'                                      # Type of window function used in the STFT
 N_MELS = 80                                                 # Number of Mel bands to generate in the Mel-spectrogram, edit it carefully.
-NFFT = 400                                                  # Number of FFT components for the STFT process, edit it carefully.
+NFFT_STFT = 400                                             # Number of FFT components for the STFT process, edit it carefully.
+NFFT_FBANK = 512                                            # Number of FFT components for the FBank process, edit it carefully.
 HOP_LENGTH = 160                                            # Number of samples between successive frames in the STFT, edit it carefully.
 VOICE_EMBED_DIM = 192                                       # Model setting.
 SAMPLE_RATE = 16000                                         # The model parameter, do not edit the value.
 PRE_EMPHASIZE = 0.97                                        # For audio preprocessing.
 SLIDING_WINDOW = 0                                          # Set the sliding window step for test audio reading; use 0 to disable.
+
+
+STFT_SIGNAL_LENGTH = INPUT_AUDIO_LENGTH // HOP_LENGTH + 1   # The length after STFT processed
+if NFFT_FBANK < NFFT_STFT:
+    NFFT_FBANK = NFFT_STFT
+if NFFT_FBANK > INPUT_AUDIO_LENGTH:
+    NFFT_FBANK = INPUT_AUDIO_LENGTH
+if HOP_LENGTH > INPUT_AUDIO_LENGTH:
+    HOP_LENGTH = INPUT_AUDIO_LENGTH
+
 
 python_package_path = site.getsitepackages()[-1]
 shutil.copyfile('./modeling_modified/DTDNN.py',  python_package_path + "/modelscope/models/audio/sv/DTDNN.py")
@@ -60,24 +71,28 @@ class PosEncoding(torch.nn.Module):
 
 
 class CAMPPLUS(torch.nn.Module):
-    def __init__(self, campplus, stft_model, nfft, n_mels, sample_rate, pre_emphasis):
+    def __init__(self, campplus, stft_model, nfft_stft, nfft_fbank, stft_signal_len, n_mels, sample_rate, pre_emphasis):
         super(CAMPPLUS, self).__init__()
         self.campplus = campplus
         self.stft_model = stft_model
         self.pre_emphasis = pre_emphasis
-        self.fbank = (torchaudio.functional.melscale_fbanks(nfft // 2 + 1, 20, sample_rate // 2, n_mels, sample_rate, None,'htk')).transpose(0, 1).unsqueeze(0)
+        self.fbank = (torchaudio.functional.melscale_fbanks(nfft_fbank // 2 + 1, 20, sample_rate // 2, n_mels, sample_rate, None,'htk')).transpose(0, 1).unsqueeze(0)
+        self.padding = torch.zeros((1, (nfft_fbank - nfft_stft) // 2, stft_signal_len), dtype=torch.int8)
         self.inv_int16 = float(1.0 / 32768.0)
         embed = torch.zeros((1, 1, campplus.model_config['anchor_size']), dtype=torch.int8)
         embed[:, :, 1::2] = 1
         self.anchors = torch.cat((embed, 1 - embed), dim=0)
-        self.campplus.backend.pos_enc_plus = PosEncoding(4096, 256)
+        self.campplus.backend.pos_enc_plus = PosEncoding(2048, 256)
 
     def forward(self, audio, voice_embed_x, voice_embed_y, control_factor):
         audio = audio.float() * self.inv_int16
         audio -= torch.mean(audio)  # Remove DC Offset
         audio = torch.cat((audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]), dim=-1)  # Pre Emphasize
         real_part, imag_part = self.stft_model(audio, 'constant')
-        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).clamp(min=1e-5).log()
+        power = real_part * real_part + imag_part * imag_part
+        if self.padding.shape[1] != 0:
+            power = torch.cat((power, self.padding[:, :, :power.shape[-1]].float()), dim=1)
+        mel_features = torch.matmul(self.fbank, power).clamp(min=1e-5).log()
         mel_features -= mel_features.mean(dim=-1, keepdim=True)
         anchors = torch.cat((voice_embed_x, voice_embed_y), dim=0) * control_factor + (self.anchors * (1 - control_factor)).float()
         output = self.campplus(mel_features, anchors)
@@ -89,13 +104,13 @@ class CAMPPLUS(torch.nn.Module):
 print('\nExport start ...\n')
 with torch.inference_mode():
     from modelscope.utils.config import Config, ConfigDict
-    custom_stft = STFT_Process(model_type='stft_B', n_fft=NFFT, hop_len=HOP_LENGTH, max_frames=0, window_type=WINDOW_TYPE).eval()  # The max_frames is not the key parameter for STFT, but it is for ISTFT.
+    custom_stft = STFT_Process(model_type='stft_B', n_fft=NFFT_STFT, hop_len=HOP_LENGTH, max_frames=0, window_type=WINDOW_TYPE).eval()  # The max_frames is not the key parameter for STFT, but it is for ISTFT.
     model = Model.from_pretrained(
         model_name_or_path=model_path,
         disable_update=True,
         device="cpu"
     ).eval()
-    campplus = CAMPPLUS(model, custom_stft,  NFFT, N_MELS, SAMPLE_RATE, PRE_EMPHASIZE)
+    campplus = CAMPPLUS(model, custom_stft, NFFT_STFT, NFFT_FBANK, STFT_SIGNAL_LENGTH, N_MELS, SAMPLE_RATE, PRE_EMPHASIZE)
     audio = torch.ones((1, 1, INPUT_AUDIO_LENGTH), dtype=torch.int16)
     voice_embed_x = torch.ones((1, 1, VOICE_EMBED_DIM), dtype=torch.float32)
     voice_embed_y = torch.ones((1, 1, VOICE_EMBED_DIM), dtype=torch.float32)
