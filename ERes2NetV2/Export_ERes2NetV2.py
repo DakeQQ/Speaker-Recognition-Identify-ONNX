74fd265c7a30e12045462fb153f7b0886abb84fd
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-15 10:03:38 +0800

    Update Export_ERes2NetV2.py

--- ERes2NetV2/Export_ERes2NetV2.py
+++ ERes2NetV2/Export_ERes2NetV2.py
@@ -2,34 +2,41 @@ import gc
 import time
 import shutil
 import site
-
 import numpy as np
 import onnxruntime
 import torch
 import torchaudio
 from pydub import AudioSegment
-from modelscope.models.base import Model
-
 from STFT_Process import STFT_Process  # The custom STFT/ISTFT can be exported in ONNX format.
 
 model_path = "/home/DakeQQ/Downloads/speech_eres2netv2_sv_zh-cn_16k-common"       # The ERes2NetV2 download path.
 onnx_model_A = "/home/DakeQQ/Downloads/ERes2NetV2_ONNX/ERes2NetV2.onnx"           # The exported onnx model path.
-test_audio = [model_path + "/examples/speaker2_a_cn_16k.wav", model_path + "/examples/speaker1_a_cn_16k.wav", model_path + "/examples/speaker1_b_cn_16k.wav"]     # The test audio list.
-
+test_audio = [model_path + "/examples/speaker2_a_cn_16k.wav", model_path + "/examples/speaker1_a_cn_16k.wav", model_path + "/examples/speaker1_b_cn_16k.wav"]    
+# The test audio list.
 
 ORT_Accelerate_Providers = []                               # If you have accelerate devices for : ['CUDAExecutionProvider', 'TensorrtExecutionProvider', 'CoreMLExecutionProvider', 'DmlExecutionProvider', 'OpenVINOExecutionProvider', 'ROCMExecutionProvider', 'MIGraphXExecutionProvider', 'AzureExecutionProvider']
                                                             # else keep empty.
 DYNAMIC_AXES = True                                         # The default dynamic_axes is the input audio length. Note that some providers only support static axes.
-INPUT_AUDIO_LENGTH = 96000 if not DYNAMIC_AXES else 320000  # Set for static axis export: the length of the audio input signal (in samples). Iy use DYNAMIC_AXES, Default to 320000, you can adjust it.
+INPUT_AUDIO_LENGTH = 160000                                 # The maximum input audio length.
 WINDOW_TYPE = 'kaiser'                                      # Type of window function used in the STFT
 N_MELS = 80                                                 # Number of Mel bands to generate in the Mel-spectrogram, edit it carefully.
-NFFT = 400                                                  # Number of FFT components for the STFT process, edit it carefully.
+NFFT_STFT = 400                                             # Number of FFT components for the STFT process, edit it carefully.
+NFFT_FBANK = 512                                            # Number of FFT components for the FBank process, edit it carefully.
 HOP_LENGTH = 160                                            # Number of samples between successive frames in the STFT, edit it carefully.
 SAMPLE_RATE = 16000                                         # The model parameter, do not edit the value.
 PRE_EMPHASIZE = 0.97                                        # For audio preprocessing.
 SLIDING_WINDOW = 0                                          # Set the sliding window step for test audio reading; use 0 to disable.
 MAX_SPEAKERS = 50                                           # Maximum number of saved speaker features.
 SIMILARITY_THRESHOLD = 0.5                                  # Threshold to determine the speaker's identity.
+
+
+STFT_SIGNAL_LENGTH = INPUT_AUDIO_LENGTH // HOP_LENGTH + 1   # The length after STFT processed
+if NFFT_FBANK < NFFT_STFT:
+    NFFT_FBANK = NFFT_STFT
+if NFFT_FBANK > INPUT_AUDIO_LENGTH:
+    NFFT_FBANK = INPUT_AUDIO_LENGTH
+if HOP_LENGTH > INPUT_AUDIO_LENGTH:
+    HOP_LENGTH = INPUT_AUDIO_LENGTH
 
 
 shutil.copyfile('./modeling_modified/ERes2NetV2.py', site.getsitepackages()[-1] + "/modelscope/models/audio/sv/ERes2NetV2.py")
@@ -43,13 +50,14 @@ def normalize_to_int16(audio):
 
 
 class ERES2NETV2(torch.nn.Module):
-    def __init__(self, eres2netv2, stft_model, nfft, n_mels, sample_rate, pre_emphasis):
+    def __init__(self, eres2netv2, stft_model, nfft_stft, nfft_fbank, stft_signal_len, n_mels, sample_rate, pre_emphasis):
         super(ERES2NETV2, self).__init__()
         self.eres2netv2 = eres2netv2
         self.stft_model = stft_model
         self.pre_emphasis = pre_emphasis
         self.cos_similarity = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-        self.fbank = (torchaudio.functional.melscale_fbanks(nfft // 2 + 1, 20, sample_rate // 2, n_mels, sample_rate, None,'htk')).transpose(0, 1).unsqueeze(0)
+        self.fbank = (torchaudio.functional.melscale_fbanks(nfft_fbank // 2 + 1, 20, sample_rate // 2, n_mels, sample_rate, None,'htk')).transpose(0, 1).unsqueeze(0)
+        self.padding = torch.zeros((1, (nfft_fbank - nfft_stft) // 2, stft_signal_len), dtype=torch.int8)
         self.inv_int16 = float(1.0 / 32768.0)
 
     def forward(self, audio, saved_embed, num_speakers):
@@ -57,7 +65,10 @@ class ERES2NETV2(torch.nn.Module):
         audio -= torch.mean(audio)  # Remove DC Offset
         audio = torch.cat((audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]), dim=-1)  # Pre Emphasize
         real_part, imag_part = self.stft_model(audio, 'constant')
-        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).clamp(min=1e-5).log()
+        power = real_part * real_part + imag_part * imag_part
+        if self.padding.shape[1] != 0:
+            power = torch.cat((power, self.padding[:, :, :power.shape[-1]].float()), dim=1)
+        mel_features = torch.matmul(self.fbank, power).clamp(min=1e-5).log()
         mel_features -= mel_features.mean(dim=-1, keepdim=True)
         embed = self.eres2netv2.forward(mel_features)
         score = self.cos_similarity(embed, saved_embed) if DYNAMIC_AXES else self.cos_similarity(embed, saved_embed[:num_speakers])
@@ -67,13 +78,13 @@ class ERES2NETV2(torch.nn.Module):
 
 print('\nExport start ...\n')
 with torch.inference_mode():
-    custom_stft = STFT_Process(model_type='stft_B', n_fft=NFFT, hop_len=HOP_LENGTH, max_frames=0, window_type=WINDOW_TYPE).eval()  # The max_frames is not the key parameter for STFT, but it is for ISTFT.
+    custom_stft = STFT_Process(model_type='stft_B', n_fft=NFFT_STFT, hop_len=HOP_LENGTH, max_frames=0, window_type=WINDOW_TYPE).eval()  # The max_frames is not the key parameter for STFT, but it is for ISTFT.
     model = Model.from_pretrained(
         model_name_or_path=model_path,
         disable_update=True,
         device="cpu",
     ).embedding_model.eval()
-    eres2netv2 = ERES2NETV2(model, custom_stft,  NFFT, N_MELS, SAMPLE_RATE, PRE_EMPHASIZE)
+    eres2netv2 = ERES2NETV2(model, custom_stft, NFFT_STFT, NFFT_FBANK, STFT_SIGNAL_LENGTH, N_MELS, SAMPLE_RATE, PRE_EMPHASIZE)
     audio = torch.ones((1, 1, INPUT_AUDIO_LENGTH), dtype=torch.int16)
     saved_embed = torch.randn((MAX_SPEAKERS, model.embed_dim), dtype=torch.float32)
     num_speakers = torch.tensor([1], dtype=torch.int64)
