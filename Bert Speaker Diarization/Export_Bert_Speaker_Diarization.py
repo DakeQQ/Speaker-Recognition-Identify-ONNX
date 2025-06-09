1a81a2e2edb2618d28afa440f06966afb64febd1
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-06-09 18:53:08 +0800

    Update Export_Bert_Speaker_Diarization.py

--- Bert Speaker Diarization/Export_Bert_Speaker_Diarization.py
+++ Bert Speaker Diarization/Export_Bert_Speaker_Diarization.py
@@ -90,9 +90,9 @@ print("\nExport Done!")
 # For Bert Model
 def tokenizer(input_string, max_input_words, is_dynamic):
     input_ids = np.zeros((1, max_input_words), dtype=np.int32)
-    punc_ids = input_ids
+    punc_ids = np.zeros((1, max_input_words), dtype=np.int32)
     input_string = re.findall(r'[\u4e00-\u9fa5]|[a-zA-Z]+|[^\w\s]', input_string.lower())
-    input_ids[0] = TOKEN_BEGIN
+    input_ids[:, 0] = TOKEN_BEGIN
     ids_len = 1
     full = max_input_words - 1
     for i in input_string:
